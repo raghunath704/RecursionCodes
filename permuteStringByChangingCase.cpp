ee7a654c035f170cd5fef782f32b2ab6bf97d5a6
Raghunath Das <74724537+raghunath704@users.noreply.github.com>
2025-07-18 11:54:02 +0530

    Create permuteStringByChangingCase.cpp

---
+++ permuteStringByChangingCase.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cctype> 
+
+using namespace std;
+
+void helper(string s, vector<string> &ans, string res, int i) {
+    if (i == s.size()) {
+        ans.push_back(res);
+        return;
+    }
+    helper(s, ans, res + s[i], i + 1);
+    helper(s, ans, res + (char)toupper(s[i]), i + 1);
+}
+
+vector<string> permutation(string s) {
+    vector<string> ans;
+    if (s.empty()) return ans;
+    helper(s, ans, "", 0);
+    return ans;
+}
+
+int main() {
+    string input;
+    cout << "Enter a string: ";
+    cin >> input;
+
+    vector<string> results = permutation(input);
+
+    cout << "Generated permutations:\n";
+    for (const string &str : results) {
+        cout << str << endl;
+    }
+
+    return 0;
+}
